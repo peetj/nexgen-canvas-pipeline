d7bf2c95ceaaad3401ac3368426ac988f115da52
Peter Januarius <pete@nexgencodecamp.com.au>
2026-03-05 20:46:20 +1100

    Add reveal-answer (show/hide) Canvas plugin

--- apps/plugins-runner/src/plugins/index.ts
+++ apps/plugins-runner/src/plugins/index.ts
@@ -1,7 +1,8 @@
 import { moduleOverviewPlugin } from "./moduleOverview.js";
+import { revealAnswerPlugin } from "./revealAnswer.js";
 import type { CanvasPlugin } from "../types.js";
 
-const plugins: CanvasPlugin[] = [moduleOverviewPlugin];
+const plugins: CanvasPlugin[] = [moduleOverviewPlugin, revealAnswerPlugin];
 
 export const pluginsById = new Map<string, CanvasPlugin>(
   plugins.map((plugin) => [plugin.id.toLowerCase(), plugin])
